f37c01da0e063f3f6c1ea3d4d14feaaa1d649ee4
Vraj Desai <68881630+vking45@users.noreply.github.com>
2023-03-08 22:29:43 +0530

    routed offers

--- src/Components/Offers.jsx
+++ src/Components/Offers.jsx
@@ -6,8 +6,8 @@ function Offers() {
         <section class="text-gray-600 body-font">
   <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Market Place</h1>
-      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas cum soluta aut possimus temporibus? Recusandae temporibus quam cupiditate rem fuga praesentium in maiores ut. Distinctio quidem rem eaque! Quis, voluptatibus..</p>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Offers</h1>
+      <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">All the available token offers are listed below : </p>
     </div>
     <div class="flex flex-wrap -m-4">
       <div class="xl:w-1/3 md:w-1/2 p-4">
